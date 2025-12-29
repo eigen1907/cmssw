ca46fc360668f5834fda2423fea6c313bbfea3d4
Jongwon Shin <eigen1907@gmail.com>
2025-12-29 16:05:13 +0900

    Update DMR Ntuplizer cfg to match CMSSW_14_0_9 env

--- DeepMuonReco/Ntuplizer/test/runDeepMuonRecoNtuplizer_cfg.py
+++ DeepMuonReco/Ntuplizer/test/runDeepMuonRecoNtuplizer_cfg.py
@@ -3,35 +3,32 @@ import FWCore.ParameterSet.Config as cms
 process = cms.Process("DeepMuonReco")
 
 process.load("FWCore.MessageService.MessageLogger_cfi")
-#process.load("Configuration.StandardSequences.GeometryRecoDB_cff")
-process.load('Configuration.Geometry.GeometryExtendedRun4D121Reco_cff')
+process.load('Configuration.Geometry.GeometryExtended2026D110Reco_cff')
 process.load("Configuration.StandardSequences.MagneticField_cff")
 process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
 
 from Configuration.AlCa.GlobalTag import GlobalTag
-process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:phase2_realistic_T33', '')
+process.GlobalTag = GlobalTag(process.GlobalTag, '140X_mcRun4_realistic_v4', '')
 
 process.maxEvents = cms.untracked.PSet(input = cms.untracked.int32(-1))
 
 process.source = cms.Source("PoolSource",
-    #fileNames = cms.untracked.vstring('file:recosim.root')
-    fileNames = cms.untracked.vstring('file:/home/joshin/workspace-gate/Store/store-hdfs/DeepMuonReco/CMSSW_16_0_0_pre2/Run4D121/tenmu-pileup/step4_recosim/output_0.root')
+    fileNames = cms.untracked.vstring('file:output.root')
 )
 
 process.load("SimTracker.TrackAssociatorProducers.quickTrackAssociatorByHits_cfi")
 process.load("SimTracker.TrackerHitAssociation.tpClusterProducer_cfi")
 process.load("SimTracker.TrackAssociation.trackingParticleRecoTrackAsssociation_cfi")
 
-process.tpClusterProducer.pixelSimLinkSrc = cms.InputTag("mixData", "Pixel")
-process.tpClusterProducer.phase2OTSimLinkSrc = cms.InputTag("mixData", "Phase2OTDigiSimLink")
-process.tpClusterProducer.stripSimLinkSrc = cms.InputTag("")
+process.tpClusterProducer.pixelSimLinkSrc = cms.InputTag("simSiPixelDigis", "Pixel")
+process.tpClusterProducer.phase2OTSimLinkSrc = cms.InputTag("simSiPixelDigis", "Tracker")
 process.tpClusterProducer.pixelClusterSrc = cms.InputTag("siPixelClusters")
 process.tpClusterProducer.phase2OTClusterSrc = cms.InputTag("siPhase2Clusters")
 
 process.load("DeepMuonReco.Ntuplizer.deepMuonRecoNtuplizer_cfi")
 
 process.TFileService = cms.Service("TFileService",
-    fileName = cms.string("output.root")
+    fileName = cms.string("ntuple.root")
 )
 
 process.p = cms.Path(
